ad192baa3d308dc1d8682f25c3d3a829178ab6e1
AdamBiu <adam.peretz@live.biu.ac.il>
2021-03-24 14:57:58 +0200

    Add connection to server and read from CSV

--- Advanced_Flight_Simulator/MainWindow.xaml.cs
+++ Advanced_Flight_Simulator/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
 namespace Advanced_Flight_Simulator
 {
     /// <summary>
@@ -23,6 +28,70 @@ namespace Advanced_Flight_Simulator
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Console.WriteLine("Start all");
+                IPAddress ipAd = IPAddress.Parse("127.0.0.1");
+                // use local m/c IP address, and 
+                // use the same in the client
+
+                /* Initializes the Listener */
+                TcpListener myList = new TcpListener(ipAd, 5400);
+
+                /* Start Listeneting at the specified port */
+                myList.Start(5);
+
+                Console.WriteLine("The server is running at port 8001...");
+                Console.WriteLine("The local End point is  :" +
+                                  myList.LocalEndpoint);
+                Console.WriteLine("Waiting for a connection.....");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine(k);
+                Console.WriteLine("Recieved...");
+
+                string fileName = "C:/Users/adamp/source/repos/Advanced_Flight_Simulator/Advanced_Flight_Simulator/reg_flight.csv";
+                using (var reader = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Console.WriteLine("Start send");
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(line);
+                        s.Send(msg);
+                        Console.WriteLine(msg);
+                        Console.WriteLine(line);
+                        Console.WriteLine("End send");
+                        System.Threading.Thread.Sleep(100);
+
+
+                    }
+                }
+
+
+
+
+
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The string was recieved by the server."));
+                Console.WriteLine("\nSent Acknowledgement");
+                /* clean up */
+                s.Close();
+                myList.Stop();
+
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine("Error..... " + e1.StackTrace);
+            }
         }
     }
 }
