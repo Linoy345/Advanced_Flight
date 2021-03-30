90070a0e97ec8264d151121d09f4b8708225195e
linoysela345@gmail.com <linoysela345@gmail.com>
2021-03-31 01:08:13 +0300

    joistic view

--- Advanced_Flight_Simulator/MainWindow.xaml.cs
+++ Advanced_Flight_Simulator/MainWindow.xaml.cs
@@ -33,13 +33,17 @@ namespace Advanced_Flight_Simulator
         }
 
         private void Button_Click(object sender1, RoutedEventArgs e)
-        {
+        {   
+            
+            ContinueWindow continueWindow = new ContinueWindow();
+            continueWindow.Show();
             int port = 5400;
             string ip = "127.0.0.1";
             Model_Server server = new Model_Server();
             server.connect(ip, port);
             Thread serverT = new Thread(server.send);
             serverT.Start();
+
         }
 
     }
