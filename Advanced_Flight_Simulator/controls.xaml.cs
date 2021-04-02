dbafcf3cc8e019e500e6423ec7ee44165bbd2050
AdamBiu <adam.peretz@live.biu.ac.il>
2021-04-02 17:04:16 +0300

    Added controls

---
+++ Advanced_Flight_Simulator/controls.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Advanced_Flight_Simulator
+{
+    /// <summary>
+    /// Interaction logic for controls.xaml
+    /// </summary>
+    public partial class Controls : UserControl
+    {
+        public Controls()
+        {
+            InitializeComponent();
+        }
+
+        private void back_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void play_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void pause_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void stop_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            slider.Value = 20;
+        }
+
+        private void next_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
