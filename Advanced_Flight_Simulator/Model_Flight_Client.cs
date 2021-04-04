2e567923b91db02ee4fb9e0c91a41d607ec7cea2
Yair Yardeni <yair.yardeni@live.biu.ac.il>
2021-04-04 18:12:54 +0300

    Server V2

---
+++ Advanced_Flight_Simulator/Model_Flight_Client.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace Advanced_Flight_Simulator
+{
+    public class Model_Flight_Client : Model_IClient
+    {
+        protected TcpClient client;
+        protected NetworkStream stream;
+        public Model_Flight_Client()
+        {
+            client = new TcpClient();
+        }
+        private bool is_connected()
+        {
+            if(client == null) { return false; }
+            return client.Connected;
+        }
+        public void connect(string ip, int port)
+        {
+            if (!is_connected())
+            {
+                try
+                {
+                    client.Connect(ip, port);
+                    stream = client.GetStream();
+                } catch(System.Net.Sockets.SocketException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }
+        }
+        public void write(string line)
+        {
+            if (is_connected())
+            {
+                try
+                {
+                    byte[] message = Encoding.ASCII.GetBytes(line);
+                    stream.Write(message, 0, message.Length);
+                }
+                catch (FileNotFoundException not_found)
+                {
+                    Console.WriteLine(not_found.ToString());
+                }
+            }
+        }
+        public string read() // Todo: need testing.
+        {
+            if (is_connected())
+            {
+                char[] data = new char[512];
+                BinaryReader reader = new BinaryReader(stream);
+                char currentC = ' ';
+                int i;
+                for (i = 0; i < data.Length && currentC != '\n'; i++) // Read one char each iteration.
+                {
+                    currentC = reader.ReadChar();
+                    data[i] = currentC;
+                }
+                return new string(data);
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public void disconnect()
+        {
+            client.Close();
+        }
+    }
+}
---
+++ Advanced_Flight_Simulator/Model_IClient.cs
@@ -0,0 +1,15 @@
+﻿namespace Advanced_Flight_Simulator {
+    interface Model_IClient
+    {
+        void connect(string ip, int port);
+        /*
+        * If connection has been established- Send to Server the given Line.
+        */
+        void write(string line);
+        /*
+        * Read one line or 512 bytes.
+        */
+        string read();
+        void disconnect();
+    }
+}
