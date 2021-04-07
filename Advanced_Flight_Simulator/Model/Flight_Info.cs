08d8cd1ad1c8f2b1cdfbc9b81ad8e40736071231
Linoy345 <72787752+Linoy345@users.noreply.github.com>
2021-04-08 01:09:36 +0300

    Add files via upload

---
+++ Advanced_Flight_Simulator/Model/Flight_Info.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+namespace Advanced_Flight_Simulator
+{
+    /*
+     * Handle Flight information- support different queries to extract data from specific attribute or specifiec row.
+     * Require csv(for data) and xml(for headers) paths.
+     */
+    class Flight_Info
+    {
+        private List<Attribute> attributes;
+        private List<Dictionary<string, string>> rows; // Keep rows in list- each row is a dictionary- key is attribute name.
+        public Flight_Info()
+        {
+            rows = new List<Dictionary<string, string>>();
+            attributes = new List<Attribute>();
+        }
+        /*
+         * Extract data from csv and xml files using thier paths.
+         */
+        public void init_Flight_Info(string csv_path)
+        {
+            extract_headers("playback_small.xml");
+            extract_values(csv_path);
+        }
+        /*
+        * Extract Headers from xml file. Headers are stored in "output"-name.
+        */
+        private void extract_headers(string xml_path)
+        {
+            XElement Xelement = XElement.Load(xml_path);
+            XDocument xDoc = XDocument.Load(xml_path);
+            IEnumerable<string> query = Xelement.Descendants("output").Descendants("name").Select(name => (string)name);
+            foreach (var name in query.ToList())
+            {
+                attributes.Add(new Attribute(name));
+            }
+        }
+        /*
+         * Extract values into attributes and rows from a path to a CSV file.
+         */
+        private void extract_values(string csv_path)
+        {
+            string[] lines = File.ReadAllLines(csv_path);
+            int coulumn_index = 0;
+            int row_index = 0;
+            string current_value;
+            string current_name;
+
+            foreach (var line in lines)
+            {
+                rows.Add(new Dictionary<string, string>());
+                string[] current_line = line.Split(',');
+                foreach (var attribute in attributes)
+                {
+                    current_name = attribute.name;
+                    if (rows[row_index].ContainsKey(attribute.name))
+                    {
+                        current_name += "2";
+                    }; // Add 2 to name if two attributes have the same name
+                    current_value = current_line[coulumn_index].ToString();
+                    attribute.add_value(current_value);
+                    rows[row_index].Add(current_name, current_value);
+                    coulumn_index++;
+                }
+                coulumn_index = 0;
+                row_index++;
+            }
+        }
+
+        /*
+         * Return a list of all the values in given attribute.
+         */
+        public List<string> get_attribute(string att_name)
+        {
+            foreach (var attribute in this.attributes)
+            {
+                if (attribute.name == att_name)
+                {
+                    return attribute.get_values();
+                }
+            }
+            return new List<string>();
+        }
+        public Dictionary<string,string> get_row(int row)
+        {
+            return this.rows[row];
+        }
+
+        public string get_value(int row, string attribute_name)
+        {
+            if(row < row_count() && rows[row].ContainsKey(attribute_name))
+            {
+                return rows[row][attribute_name];
+            }
+            return string.Empty;
+               
+        }
+
+        /*
+         * Convert given row values into a single string wheres each value is seperated by ",".
+         */
+        public string get_row_string(int row)
+        {
+            return string.Join(",", rows[row].Select(x => x.Value).ToArray()) + "\r\n";
+        }
+        public int row_count()
+        {
+            return this.rows.Count();
+        }
+        public int attribute_count()
+        {
+            return this.attributes.Count();
+        }
+
+    }
+
+
+}
---
+++ Advanced_Flight_Simulator/Model/MyFlightModel.cs
@@ -0,0 +1,315 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Advanced_Flight_Simulator
+{
+    class MyFlightModel : IFlightModel
+    {
+        private Flight_Info info;
+        private IClient client;
+
+        volatile private bool shouldStop;
+        volatile private int frameId;
+
+        private double throttle;
+        private double rudder;
+        private double aileron;
+        private double elevator;
+        private double x;
+        private double y;
+
+        private double frequency;
+        volatile private int rowCount;
+        private double yaw;
+        private double roll;
+        private double pitch;
+        private double direction;
+        private double altitude;
+        private double speed;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MyFlightModel(IClient client)
+        {
+            FrameId = 0;
+            Frequency = 1;
+            info = new Flight_Info();
+            //info.init_Flight_Info("reg_flight.csv"); // TODO: INIT in open method
+            this.client = client;
+            ShouldStop = false;
+
+
+            x = 0;
+            y = 0;
+        }
+
+        public bool ShouldStop
+        {
+            get { return shouldStop; }
+            set
+            {
+                shouldStop = value;
+                NotifyPropertyChanged("ShouldStop");
+            }
+        }
+        public double Direction
+        {
+            get { return direction; }
+            set
+            {
+                direction = value;
+                NotifyPropertyChanged("Direction");
+            }
+        }
+
+        public double Yaw
+        {
+            get { return yaw; }
+            set
+            {
+                yaw = value;
+                NotifyPropertyChanged("Yaw");
+            }
+        }
+        public double Roll
+        {
+            get { return roll; }
+          
+            set 
+            {
+                roll = value;
+                NotifyPropertyChanged("Roll");
+            }
+
+        }
+        public double Pitch
+        {
+            get { return pitch; }
+            set
+            {
+                pitch = value;
+                NotifyPropertyChanged("Pitch");
+            }
+        }
+        public double Altitude
+        {
+            get { return altitude; }
+            set
+            {
+                altitude = value;
+                NotifyPropertyChanged("Altitude");
+            }
+        }
+        public double Speed
+        {
+            get { return speed; }
+            set
+            {
+                speed = value;
+                NotifyPropertyChanged("Speed");
+            }
+        }
+        public void RefreshIndices()
+        {
+            Speed = Convert.ToDouble(info.get_value(FrameId, "airspeed-kt"));
+            Altitude = Convert.ToDouble(info.get_value(FrameId, "altitude-ft"));
+            Pitch = Convert.ToDouble(info.get_value(FrameId, "pitch-deg"));
+            Roll = Convert.ToDouble(info.get_value(FrameId, "roll-deg"));
+            Yaw = Convert.ToDouble(info.get_value(FrameId, "side-slip-deg"));
+            Direction = Convert.ToDouble(info.get_value(FrameId, "heading-deg"));
+        }
+        public int FrameId
+        {
+            get { return frameId; }
+            set
+            {
+                frameId = value;
+                NotifyPropertyChanged("FrameId");
+            }
+        }
+
+        public double Aileron
+        {
+            get
+            {
+                return aileron;
+            }
+            set
+            {
+                aileron = value;
+                NotifyPropertyChanged("Aileron");
+            }
+        }
+        public double Elevator
+        {
+            get
+            {
+                return elevator;
+            }
+            set
+            {
+                elevator = value;
+                NotifyPropertyChanged("Elevator");
+            }
+        }
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+                NotifyPropertyChanged("X");
+            }
+        }
+
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+                NotifyPropertyChanged("Y");
+            }
+        }
+        
+
+        public double Rudder
+        {
+            get
+            {
+                return rudder;
+            }
+            set
+            {
+                if (value >= -1 && value <= 1)
+                {
+                    rudder = value;
+                    NotifyPropertyChanged("Rudder");
+                }
+            }
+        }
+        public double Throttle
+        {
+            get
+            {
+                return throttle;
+            }
+            set
+            {
+                if(value >= -1 && value <= 1)
+                {
+                    throttle = value;
+                    NotifyPropertyChanged("Throttle");
+                }
+                
+            }
+        }
+
+
+        public double Frequency
+        {
+            get { return frequency; }
+            set
+            {
+                frequency = 100 / value;
+                NotifyPropertyChanged("Frequency");
+            }
+        }
+
+        public int RowCount
+        {
+            get { return info.row_count(); }
+            set
+            {
+                rowCount = value;
+                NotifyPropertyChanged("RowCount");
+            }
+        }
+
+
+        public void changeFrame(int id)
+        {
+            FrameId = id;
+        }
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+        public void stopFrame()
+        {
+            ShouldStop = true;
+        }
+
+        public void continueFrame()
+        {
+            ShouldStop = false;
+        }
+
+        public void connect(string ip, int port)
+        {
+            client.connect(ip, port);
+        }
+
+        public void disconnect()
+        {
+            client.disconnect();
+            stopFrame();
+            FrameId = 0;
+        }
+        public void updateJoistick() {
+            Rudder = Convert.ToDouble(info.get_value(FrameId, "rudder"));
+            Throttle = Convert.ToDouble(info.get_value(FrameId, "throttle"));
+            Aileron = Convert.ToDouble(info.get_value(FrameId, "aileron"));
+            Elevator = Convert.ToDouble(info.get_value(FrameId, "elevator"));
+
+            X = Aileron * 90;
+            Y = Elevator * 90;
+        }
+        public void sendFrame()
+        {
+            if (!ShouldStop)
+            {
+                client.write(info.get_row_string(FrameId));
+                RefreshIndices();
+                updateJoistick();
+                FrameId++;
+            }
+        }
+
+        public int attributeCount()
+        {
+            return info.attribute_count();
+        }
+        public void init(string path)
+        {
+            info.init_Flight_Info(path);
+        }
+
+        public void start()
+        {
+            new Thread(delegate ()
+            {
+                while (frameId < info.row_count())
+                {
+                    sendFrame();
+                    // the same for the other sensors properties
+                    Thread.Sleep((int)Frequency);// read the data in 4Hz
+                }
+            }).Start();
+        }
+    }
+}
